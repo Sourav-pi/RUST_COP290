ece1c5e5c3c3051f4fabc1270d85893d8eff68bd
Sourav Kumar Patel <116533955+Sourav-pi@users.noreply.github.com>
2025-03-30 23:41:06 +0530

    project structure established (updated)

---
+++ cli/src/lib.rs
@@ -0,0 +1,2 @@
+// this function will be called from the app to run the cli
+pub fn run() {}
---
+++ cores/src/main.rs
@@ -0,0 +1,4 @@
+// Use main file for testing
+// run from home directory : cargo run -p cores
+fn main() {
+}
