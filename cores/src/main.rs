3a95b0e6513e2b2eb2f92c30d287a06e70c8fb22
Aditya-vegad <cs1230819@iitd.ac.in>
2025-04-06 16:43:59 +0530

    bugged dependency

--- cores/src/main.rs
+++ cores/src/main.rs
@@ -8,24 +8,8 @@ use sheet::{Sheet, Cell};
 
 fn main(){
     let mut test_sheet = Sheet::create_sheet(6, 6); 
-    let target_row:usize=1;
-    let target_col:usize=1;
-    let input= String::from("AVG(A2:A4)");
-    let command = parse_formula(&input);
-    println!("command {}", command.pr());
-    test_sheet.set_dependicies_cell(target_row, target_col, command);
-    let input1= String::from("A3-A4");
-    let command1 = parse_formula(&input1);
-    test_sheet.set_dependicies_cell(1, 2, command1);
-    let input2= String::from("7");
-    let command2 = parse_formula(&input2);
-    test_sheet.set_dependicies_cell(1, 3, command2);
-    let input3= String::from("A5/B3");
-    let command3 = parse_formula(&input3);
-    test_sheet.set_dependicies_cell(1, 4, command3);
-    let topo_vec=test_sheet.toposort(100001);
-    println!("Toposort: {:?}", topo_vec);
-    test_sheet.update_cell(topo_vec);
-    println!("value {:?}", test_sheet.grid[1][1].value);
-    println!("value {:?}", test_sheet.grid[1][1].formula.flag.is_div_by_zero());
+    test_sheet.update_cell_data(1,1, String::from("A2+A3"));
+    test_sheet.update_cell_data(1,2, String::from("3"));
+    println!("A1: {}", test_sheet.get_value(1, 1));
+    
 }
