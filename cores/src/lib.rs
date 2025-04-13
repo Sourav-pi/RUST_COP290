aeb1815d3fe15d723f2f3555ba02e1284d5b7feb
Aditya-vegad <cs1230819@iitd.ac.in>
2025-04-13 12:20:35 +0530

    read_and write file implemented successfully

--- cores/src/lib.rs
+++ cores/src/lib.rs
@@ -1,7 +1,7 @@
 mod parse;
 mod sheet;
 mod write_csv;
+mod read_csv;
 
 pub use parse::convert_to_index;
 pub use sheet::Sheet;
-pub use write_csv::write_csv;
--- cores/src/main.rs
+++ cores/src/main.rs
@@ -5,7 +5,7 @@ mod parse;
 mod sheet;
 use sheet::Sheet;
 mod write_csv;
-use write_csv::write_csv;
+mod read_csv;
 
 fn main(){
     let mut test_sheet = Sheet::new(6, 6); 
@@ -13,10 +13,15 @@ fn main(){
     test_sheet.update_cell_data(2,2, String::from("500"));
     test_sheet.update_cell_data(2,1,String::from("B1+B5"));
     println!("{}", test_sheet.get_value(1, 1));
-    let res=write_csv(test_sheet, "/Users/aditya/Downloads/sem4/cop290/temp.csv");
+    let res=test_sheet.write_csv( "/Users/aditya/Downloads/sem4/cop290/temp.csv");
     match res {
         Ok(_) => println!("CSV file written successfully."),
         Err(e) => println!("Error writing CSV file: {}", e),
     } 
-    
+    let mut new_sheet = Sheet::new(6, 6);
+    let result = new_sheet.read_file("/Users/aditya/Downloads/sem4/cop290/temp.csv");
+    match result {
+        Ok(()) => println!("CSV file read successfully."),
+        Err(e) => println!("Error reading CSV file: {}", e),
+    }
 }
