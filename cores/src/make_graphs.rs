f6669d6a3ffcc885058e7180235c880cea1bbf7e
Aditya-vegad <cs1230819@iitd.ac.in>
2025-04-17 16:44:51 +0530

    graph done

--- cores/src/make_graphs.rs
+++ cores/src/make_graphs.rs
@@ -1,8 +1,7 @@
 use charming::{Chart, component::{Axis,Title,Legend}, element::{AxisType,Emphasis, ItemStyle, Orient, Tooltip, Trigger, Symbol}, series::{Bar, Line,pie}};
 use crate::sheet::Sheet;
 use crate::parse::convert_to_index;
-use charming::datatype::DataPoint;
-use charming::datatype::CompositeValue;
+use std::{cmp::max, vec};
 
 fn parse_range(range: &str) -> Result<((usize, usize), (usize, usize)), String> {
     let parts: Vec<&str> = range.split(':').collect();
@@ -29,6 +28,9 @@ fn parse_range(range: &str) -> Result<((usize, usize), (usize, usize)), String> 
 }
 
 fn parse_lables(labels: &str) -> Vec<String> {
+    if labels.is_empty() {
+        return Vec::new();
+    }
     labels.split(',').map(|s| s.trim().to_string()).collect()
 }
 impl Sheet {
@@ -42,7 +44,19 @@ impl Sheet {
     ) -> Result<String, String> {
 
         let (start, end) = parse_range(range)?;
-        let x_labels = parse_lables(x_labels);
+        let label_x: Vec<String>;
+        if x_labels.is_empty() {
+            let temp_range=max(end.0-start.0+1, end.1-start.1+1);
+            let mut temp_vec:Vec<String> = Vec::new();
+            for i in 1..(temp_range+1){
+                temp_vec.push(format!("{}",i));
+            }
+            label_x = temp_vec;
+
+        }
+        else{
+            label_x = parse_lables(x_labels);
+        }
         let mut values = Vec::new();
         for i in start.0..=end.0 {
             for j in start.1..=end.1 {
@@ -50,7 +64,7 @@ impl Sheet {
             }
         };
         Ok(Chart::new()
-        .x_axis(Axis::new().type_(AxisType::Category).data(x_labels))
+        .x_axis(Axis::new().type_(AxisType::Category).data(label_x))
         .title(Title::new().text(title))
         .y_axis(Axis::new().name(y_lable).type_(AxisType::Value))
         .series(Line::new().data(values)).to_string())
@@ -63,8 +77,22 @@ impl Sheet {
         y_lable: &str,
         title: &str,
     ) -> Result<String, String> {
+        println!("{}",x_labels);
         let (start, end) = parse_range(range)?;
-        let x_labels = parse_lables(x_labels);
+        let label_x;
+        if x_labels.is_empty() {
+            let temp_range=max(end.0-start.0+1, end.1-start.1+1);
+            let mut temp_vec:Vec<String> = Vec::new();
+            for i in 1..(temp_range+1){
+                temp_vec.push(format!("{}",i));
+            }
+            label_x = temp_vec;
+
+        }
+        else{
+            label_x = parse_lables(x_labels);
+            println!("label_x: {:?}", label_x);
+        }
         let mut values = Vec::new();
         for i in start.0..=end.0 {
             for j in start.1..=end.1 {
@@ -72,7 +100,7 @@ impl Sheet {
             }
         };
         Ok(Chart::new()
-        .x_axis(Axis::new().type_(AxisType::Category).data(x_labels))
+        .x_axis(Axis::new().type_(AxisType::Category).data(label_x))
         .title(Title::new().text(title))
         .y_axis(Axis::new().name(y_lable).type_(AxisType::Value))
         .series(Bar::new().data(values)).to_string())
@@ -84,37 +112,40 @@ impl Sheet {
         title: &str,
     ) -> Result<String, String> {
         let (start, end) = parse_range(range)?;
-        let x_labels = parse_lables(x_labels);
+        let mut x_labels = parse_lables(x_labels);
+        println!("x_labels: {:?}", x_labels);
         let mut values:Vec<i32> = Vec::new();
         let mut cnt=0;
         
-        for i in start.0..=end.0 {
-            for j in start.1..=end.1 {
-                if cnt<x_labels.len(){
-                    values.push(self.grid[i][j].value.clone());
-                    cnt+=1;
+        if start.0==end.0 {
+            for i in start.1..=end.1 {
+                values.push(self.grid[start.0][i].value.clone());
+                if x_labels.len()<=cnt{
+                    x_labels.push(format!("{}",cnt+1));
                 }
-                else{
-                    values.push(self.grid[i][j].value.clone());
-                }
+                cnt+=1;
             }
-        };
-    let data: Vec<DataPoint> = values.into_iter()
-    .zip(x_labels.into_iter())
-    .map(|(v, label)| {
-        DataPoint::from(
-            CompositeValue::from(vec![
-                CompositeValue::from(v),
-                CompositeValue::from(label),
-            ])
-        )
-    })
-    .collect();
+        }
+        else if start.1==end.1 {
+            for i in start.0..=end.0 {
+                values.push(self.grid[i][start.1].value.clone());
+                if x_labels.len()<=cnt{
+                    x_labels.push(format!("{}",cnt+1));
+                }
+                cnt+=1;
+            }
+        }
+        else{
+            return Err("Invalid range".to_string());
+        }
+    let data: Vec<(i32,String)> = values.into_iter()
+    .zip(x_labels.into_iter()).collect();
+    println!("{:?}",data);
         Ok(Chart::new()
         .tooltip(Tooltip::new().trigger(Trigger::Item))
-        .legend(Legend::new().orient(Orient::Vertical).left("left"))
+        .legend(Legend::new().orient(Orient::Vertical).right("right"))
         .series(pie::Pie::new()
-            .radius(50)
+            .radius(100)
             .center(vec!["50%", "50%"])
             .data(data)
             .emphasis(Emphasis::new().item_style(ItemStyle::new().shadow_color("rgba(0, 0, 0, 0.5)").shadow_offset_x(0).shadow_offset_y(5).shadow_blur(10)))
@@ -133,10 +164,10 @@ impl Sheet {
         let (start1, end1) = parse_range(rangex)?;
         let (start2, end2) = parse_range(rangey)?;
 
-        let diff1= start1.0 - end1.0;
-        let diff2= start1.1 - end1.1;
-        let diff3= start2.0 - end2.0;
-        let diff4= start2.1 - end2.1;
+        let diff1= (end1.0-start1.0) as i32;
+        let diff2= (end1.1 -start1.1 ) as i32;
+        let diff3= (end2.0-start2.0 ) as i32;
+        let diff4= (end2.1-start2.1) as i32;
         if diff1!=diff3 || diff2!=diff4{
             return Err("Invalid range".to_string());
         }
@@ -144,8 +175,8 @@ impl Sheet {
         for i in 0..diff1+1 {
             for j in 0..diff2+1 {
                 let mut temp_vec:Vec<i32> = Vec::new();
-                temp_vec.push(self.grid[start1.0+i][start1.1+j].value.clone());
-                temp_vec.push(self.grid[start2.0+i][start2.1+j].value.clone());
+                temp_vec.push(self.grid[start1.0+i as usize][start1.1+j as usize].value.clone());
+                temp_vec.push(self.grid[start2.0+i as usize][start2.1+j as usize].value.clone());
                 values.push(temp_vec);
             }
         };
@@ -157,3 +188,33 @@ impl Sheet {
         .to_string())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_line_graph() {
+        let mut test_sheet = Sheet::new(6, 6);
+        test_sheet.update_cell_data(1, 1, String::from("A2+A3"));
+        test_sheet.update_cell_data(2, 1, String::from("90"));
+        test_sheet.update_cell_data(3, 1, String::from("50"));
+        test_sheet.update_cell_data(4, 1, String::from("A1+A2"));
+        test_sheet.update_cell_data(5, 1, String::from("-5"));
+        test_sheet.update_cell_data(6, 1, String::from("6"));
+        let result = test_sheet.line_graph("A1:A6", "A2,A3,A4,A5,A6", "Y Axis", "Line Graph");
+        assert!(result.is_ok());
+    }    
+}
+    #[test]
+    fn test_scatter_graph() {
+        let mut test_sheet = Sheet::new(6, 6);
+        test_sheet.update_cell_data(1, 1, String::from("A2+A3"));
+        test_sheet.update_cell_data(2, 1, String::from("90"));
+        test_sheet.update_cell_data(3, 1, String::from("50"));
+        test_sheet.update_cell_data(4, 1, String::from("A1+A2"));
+        test_sheet.update_cell_data(5, 1, String::from("-5"));
+        test_sheet.update_cell_data(6, 1, String::from("6"));
+        let result = test_sheet.scatter_graph("A1:A6", "B1:B6", "Scatter Graph", "X Axis", "Y Axis");
+        assert!(result.is_ok());
+    }   
